616dfdd2c59776ce918763d32832dfc190f7a3b4
slyzah <slyzah@gmail.com>
2015-11-07 22:59:08 +1100

    add devise for users

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,8 @@ module Pinteresting
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    # Precompile to false not sure if needed for Rails 4.2.x related to gem Devise
+    config.assets.initialize_on_precompile = false
   end
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Default url for Devise Gem
+  config.action_mailer.default_url_options = { host: 'http://slz-pinteresting.herokuapp.com/' }
 end
